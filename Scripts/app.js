5ba0e86801339c878111e9aee72761d10cb2eff5
Federico Paras Sarmiento <federicoparassarmiento@yahoo.com>
2016-09-01 05:10:39 +0800

    up

--- Scripts/app.js
+++ Scripts/app.js
@@ -49,6 +49,20 @@
 		};
 	});
 	
+	app.directive('productGallery', function(){
+    return{
+      restrict: 'E',
+      templateUrl: 'product-gallery.html',
+      controller: function(){
+        this.current = 0;
+    this.setCurrent = function(imageNumber){
+      this.current = imageNumber || 0;
+    };
+      },
+      controllerAs: 'gallery'
+    };
+  });
+	
 	app.directive('productTabs', function(){
 		return{
 			restrict:'E',
